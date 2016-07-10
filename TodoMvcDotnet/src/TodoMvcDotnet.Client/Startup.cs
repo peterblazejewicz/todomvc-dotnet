4d3d6f122cfb21aa54345688f3ca83ff50bf561e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-10 14:52:43 +0200

    Update Dotnet static html pages implementation

--- TodoMvcDotnet/src/TodoMvcDotnet.Client/Startup.cs
+++ TodoMvcDotnet/src/TodoMvcDotnet.Client/Startup.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace TodoMvcDotnet.Client
 {
@@ -17,8 +16,12 @@ namespace TodoMvcDotnet.Client
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets();
+            }
+            builder.AddEnvironmentVariables();
             Configuration = builder.Build();
         }
 
@@ -27,8 +30,16 @@ namespace TodoMvcDotnet.Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // add CORS support (used during development)
+            services.AddCors();
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(o =>
+                {
+                    o.SerializerSettings.Formatting = Formatting.Indented;
+                    o.SerializerSettings.ContractResolver =
+                        new CamelCasePropertyNamesContractResolver();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +47,31 @@ namespace TodoMvcDotnet.Client
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.MapWhen(context =>
+            {
+                var path = context.Request.Path.Value;
+                if (path.Contains("/api/")) return false;
+                return (!path.Contains("."));
+            }, aBranch =>
+            {
+                aBranch.Use((context, next) =>
+                {
+                    context.Request.Path = new PathString("/index.html");
+                    return next();
+                });
+                aBranch.UseStaticFiles();
+            });
+            // allow CORS during development
+            if (env.IsDevelopment())
+            {
+                app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            }
             app.UseMvc();
         }
     }
