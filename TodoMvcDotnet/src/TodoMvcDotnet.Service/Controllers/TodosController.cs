4d7bc680bbd5c5626684a0d50d9ec062c29c1533
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-10 23:02:25 +0200

    Modify dotnet and VSCode

--- TodoMvcDotnet/src/TodoMvcDotnet.Service/Controllers/TodosController.cs
+++ TodoMvcDotnet/src/TodoMvcDotnet.Service/Controllers/TodosController.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using TodoMvcDotnet.Service.Models;
 using System.Linq;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace TodoMvcDotnet.Service.Controllers
 {
     [Route("api/[controller]")]
