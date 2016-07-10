5351e4f2d127f01e6498b82f0862c94cc5e30f2f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-10 20:59:26 +0200

    Add scaffolded migrations
    So project is ready to be run

---
+++ TodoMvcDotnet/src/TodoMvcDotnet.Client/Migrations/TodosContextModelSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TodoMvcDotnet.Service.Models;
+
+namespace TodoMvcDotnet.Client.Migrations
+{
+    [DbContext(typeof(TodosContext))]
+    partial class TodosContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("TodoMvcDotnet.Service.Models.Todo", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("Complete");
+
+                    b.Property<string>("Title")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Todos");
+                });
+        }
+    }
+}
