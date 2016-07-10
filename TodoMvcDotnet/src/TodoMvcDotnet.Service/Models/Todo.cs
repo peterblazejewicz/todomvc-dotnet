9a8cc2fba6e85ef18ffc0e5fee7ddde7ccafc782
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-10 15:14:43 +0200

    Add service layer project
    - scaffold new project
    - update .Client project

---
+++ TodoMvcDotnet/src/TodoMvcDotnet.Service/Models/Todo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoMvcDotnet.Service.Models
+{
+    public class Todo
+    {
+        public Todo()
+        {
+        }
+    }
+}
